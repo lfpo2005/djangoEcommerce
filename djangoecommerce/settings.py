6df04ea139a580faeb300e5635c46ca4dfc72cfc
lfpo2005 <lfpo2005@yahoo.com.br>
2019-08-10 21:16:16 -0300

    Configuring

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'ecommercejaleco.herokuapp.com']
 
 
 # Application definition
