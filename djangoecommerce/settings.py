4e1add0d926ebaec48e02e79bf481b696045fac9
lfpo2005 <lfpo2005@yahoo.com.br>
2019-08-20 20:26:12 -0300

    Merge remote-tracking branch 'origin/master' Merge remote-tracking branch 'origin/master' Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	catalog/models.py
    #	catalog/urls.py
    
    # Conflicts:
    #	catalog/models.py
    #	catalog/urls.py
    
    # Conflicts:
    #	catalog/models.py
    #	catalog/urls.py
    
    # Conflicts:
    #	catalog/models.py
    #	catalog/urls.py

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+   # 'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -124,7 +124,3 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-try:
-    from .local_settings import *
-except ImportError:
-    pass
